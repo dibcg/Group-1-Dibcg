6dbe47313043a6183ccdc369aa6f6e410f77a0e4
dibcg <146076706+dibcg@users.noreply.github.com>
2023-11-23 04:56:44 +0000

    last

--- week_9/pointer_arithmetic-1.c
+++ week_9/pointer_arithmetic-1.c
@@ -8,9 +8,9 @@ int main()
 
     printf ( "\nAddress[0] = %d\n", *address);
 
-    printf ( "\nAddress[1] = %d\n", *address+1);
+    printf ( "\nAddress[1] = %d\n", *(address+1));
 
-    printf ("\n Address[2] = %d\n", *(address+1));
+    printf ("\n Address[2] = %d\n", *(address+2));
 
     
 
